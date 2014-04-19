dec7b44bb2a8685825db31ea08661a518fa6e49a
Aimee <AimeeKnight83@gmail.com>
2014-04-19 10:29:28 -0500

    add trailing spaces to month array

--- lib/month.rb
+++ lib/month.rb
@@ -1,3 +1,62 @@
+# Year
+#   Stores an array of Month
+# YearFormatter
+#   Take an array of Months
+#   Go through each array
+#   Generate the string
+#   Return it
+# Month
+#   Array of arrays of days
+# MonthFormatter
+#   Take a Month
+#   Grab the array of arrays of days
+#   Generate a single string from that array
+#   Return it
+# ZellerCalculator
+#   Give it month and year and it returns the zeller month/year
+
+class ZellerCalculator
+  MONTHS = ['January','February','March','April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+  attr_accessor :original_month,
+                :original_year,
+                :original_day
+
+  def initialize(month, year, day = 1)
+    #self.original_month = month
+    self.original_year = year
+    self.original_day = day
+    self.original_month = MONTHS.each_index.find { |index| MONTHS[index] == month }
+  end
+
+  def calculate
+    calculated_day = original_day
+    calculated_month = ((zeller_month + 1) * 26) / 10
+    calculated_year = zeller_year + (zeller_year / 4) + 6 * (zeller_year / 100) + (zeller_year / 400)
+
+    (calculated_day + calculated_month + calculated_year) % 7
+  end
+
+  private
+
+  def zeller_year
+    if zeller_month == 13 || zeller_month == 14
+      original_year - 1
+    else
+      original_year
+    end
+  end
+
+  def zeller_month
+    if original_month < 2
+      original_month + 13
+    else
+      original_month + 1
+    end
+  end
+end
+
+################################################################################################
+
 class Month
   include Enumerable
 
@@ -5,62 +64,39 @@ class Month
   MONTH_LENGTHS = { 'January' => 31, 'February' => 28, 'March' => 31, 'April' => 30, 'May' => 31, 'June' => 30, 'July' => 31, 'August' => 31, 'September' => 30, 'October' => 31, 'November' => 30, 'December' => 31 }
   SPACES_BEFORE = [6, 0, 1, 2, 3, 4, 5]
   SPACES_AFTER  = [0, 6, 5, 4, 3, 2, 1]
+  TOTAL_SPACE = 42
 
   attr_accessor :month
   attr_reader :start
 
-  def initialize (month, day, year)
+  def initialize (month, year, day = 1)
     @month = month
     @day = day.to_i
     @year = year.to_i
-    @start = self.zeller
+    @start = ZellerCalculator.new(@month, @year).calculate
     @total_days = MONTH_LENGTHS[@month]
     @num_spaces_before = SPACES_BEFORE[@start]
-    @month_array = self.build_month
-    @month_int
+    @month_array = self.prep_month
   end
 
   def leap_year?
-    # increment year back to actual year since we set it back for zellers
-    year = @year + 1
-    (year % 4 == 0) and !(year % 100 == 0) || (year % 400 == 0)
+    (@year % 4 == 0) and !(@year % 100 == 0) || (@year % 400 == 0)
   end
 
-  def prep_zeller_month
-    @idx = MONTHS.each_index.select { |index| MONTHS[index] == @month }
-    if @idx[0] < 2
-      @month_int = @idx[0] += 13
-    else
-      @month_int = @idx[0] += 1
-    end
-  end
-
-  def prep_zeller_year
-    @year -= 1 if @month_int == 13 or @month_int == 14
-  end
-
-  def zeller
-    self.prep_zeller_month
-    self.prep_zeller_year
-    h = (@day + ((@month_int +1) * 26) / 10) + @year + (@year / 4 ) + 6 * (@year / 100) + (@year/400)
-    h = h % 7
-    @start = h
-  end
-
-  def build_month
-    newline_counter = 0
-    year = @year
-    year = @year + 1 if @month == 'February' or @month == 'January'
-    @total_days += 1 if self.leap_year? and @month == 'February' 
+  def prep_month
     month_array = []
-    header = "#{month} #{year}".center(20).rstrip
-    days = 'Su Mo Tu We Th Fr Sa'
-
+    header = "#{@month} #{@year}".center(20).rstrip
     puts "#{header}"
+    days = 'Su Mo Tu We Th Fr Sa'
     puts "#{days}"
     @num_spaces_before.times do
       month_array << "   "
     end
+    build_month month_array
+  end
+
+  def build_month(month_array)
+    @total_days += 1 if self.leap_year? and @month == 'February' 
 
     @total_days.times do |i|
       if i < 9
@@ -69,6 +105,15 @@ class Month
         month_array << "#{i + 1} "
       end
     end
+    fill_in_remaining month_array
+  end
+
+  def fill_in_remaining(month_array) 
+    filled_in_currently = @total_days + @num_spaces_before
+    to_go = TOTAL_SPACE - filled_in_currently
+    to_go.times do
+      month_array << "   "
+    end
     @month_array = month_array
   end
 
@@ -76,7 +121,6 @@ class Month
     @month_array = @month_array.each_slice(7) do |x|
       puts "#{x.join}".rstrip
     end
-    puts
   end
 
 end
--- test/test_month.rb
+++ test/test_month.rb
@@ -1,26 +1,54 @@
 require 'month'
 require 'test/unit'
 
-#class MonthTest < MiniTest::Unit::TestCase
-class MonthTest < Test::Unit::TestCase
+class ZellerCalculatorTest < Test::Unit::TestCase
 
   def test_01_01_2014
-    month = Month.new('January', 01, 2014)
-    assert_equal 4, month.start
+    zeller = ZellerCalculator.new('January', 2014, 01)
+    assert_equal 4, zeller.calculate
     #Wednesday
   end
+
+  def test_02_28_2014
+    zeller = ZellerCalculator.new('February', 2014, 28)
+    assert_equal 6, zeller.calculate
+    #Friday
+  end
+
+  def test_02_01_2014
+    zeller = ZellerCalculator.new('February', 2014, 01)
+    assert_equal 0, zeller.calculate
+    #Saturday
+  end
+
+  def test_03_01_2014
+    zeller = ZellerCalculator.new('March', 2014, 01)
+    assert_equal 0, zeller.calculate
+    #Saturday
+  end
+
+  def test_04_01_2014
+    zeller = ZellerCalculator.new('April', 2014, 01)
+    assert_equal 3, zeller.calculate
+    #Tuesday
+  end
+
+  def test_02_29_2016
+    zeller = ZellerCalculator.new('February', 2016, 29)
+    assert_equal 2, zeller.calculate
+    #Monday
+  end
+
+end
+
+#class MonthTest < MiniTest::Unit::TestCase
+class MonthTest < Test::Unit::TestCase
 
   def test_01_01_2014_to_string
     month = Month.new('January', 01, 2014)
     expected = `cal January 2014`
     actual = `ruby ./lib/cal January 2014`
     assert_equal expected, actual
-  end
-
-  def test_02_28_2014
-    month = Month.new('February', 28, 2014)
-    assert_equal 6, month.start
-    #Friday
   end
 
   def test_02_28_2014_to_string
@@ -30,18 +58,6 @@ class MonthTest < Test::Unit::TestCase
     assert_equal expected, actual
   end
 
-  def test_02_01_2014
-    month = Month.new('February', 01, 2014)
-    assert_equal 0, month.start
-    #Saturday
-  end
-
-  def test_03_01_2014
-    month = Month.new('March', 01, 2014)
-    assert_equal 0, month.start
-    #Saturday
-  end
-
   def test_03_01_2014_to_string
     month = Month.new('March', 01, 2014)
     expected = `cal March 2014`
@@ -49,22 +65,17 @@ class MonthTest < Test::Unit::TestCase
     assert_equal expected, actual
   end
 
-  def test_04_01_2014
-    month = Month.new('April', 01, 2014)
-    assert_equal 3, month.start
-    #Tuesday 
+  def test_08_01_2014_to_string
+    month = Month.new('August', 01, 2014)
+    expected = `cal August 2014`
+    actual = `ruby ./lib/cal August 2014`
+    assert_equal expected, actual
   end
 
-  def test_02_29_2016
-    month = Month.new('February', 29, 2016)
-    assert_equal 2, month.start
-    #Monday 
-  end
-
-  #def test_invalid_entry
-   # asert_raises ArgumentError do
-      #Month.start(022012)
-    #end
-  #end
+ #def test_invalid_entry
+  # asert_raises ArgumentError do
+     #Month.start(022012)
+   #end
+ #end
 
 end
